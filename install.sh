e6f79f8a1b11c0e2ad60d469bb70bfe1d94c08ab
r4ds3c <323016+r4ds3c@users.noreply.github.com>
2024-02-20 12:58:00 +0100

    Update install.sh
    remove i3-gaps install because of merge with i3

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ apt-get upgrade -y
 apt-get install -y git build-essential autoconf libnl-genl-3-dev pkg-config libcairo2-dev feh libxcb-composite0-dev xcb-proto libxcb-ewmh-dev python-xcbgen terminator cmake compton libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
 apt-get install -y i3 # to get dependencies
 mkdir gui && cd gui
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps
+git clone https://github.com/i3/i3
+cd i3
 autoreconf --force --install
 rm -rf build/
 mkdir -p build && cd build/
